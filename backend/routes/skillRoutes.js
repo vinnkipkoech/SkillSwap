4d6c2d4273e62b8c49e96c6aa96699e5811676ae
Lincon Thuita Mwangi <lincon.mwangi@student.moringaschool.com>
2026-05-13 16:51:59 +0300

    Backend API and routes ready for frontend

--- backend/routes/skillRoutes.js
+++ backend/routes/skillRoutes.js
@@ -5,4 +5,14 @@ router.get('/', (req, res) => {
     res.json({ message: "API is working!" });
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const newSkill = new Skill(req.body);
+        const savedSkill = await newSkill.save();
+        res.status(201).json(savedSkill);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
 module.exports = router;
